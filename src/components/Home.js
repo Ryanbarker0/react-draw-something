0527a1ba86b2d43335e7a2d6ff53b6aa60dfb4b0
Ryan <ryangeorgebarker0@gmail.com>
2018-11-29 17:57:30 +0000

    testing

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
+import CanvasDraw from "react-canvas-draw";
 
 class Home extends Component {
 
     state = {
-
+        color: "#0000FF",
+        width: 400,
+        height: 400,
+        "savedCanvas": undefined
     }
+
+    getRandomWord = () => {
+        const words = ['Rabbit', 'Cat', 'Dog', 'Wolf', 'Tree']
+        return words[Math.floor(Math.random() * words.length)]
+    }
+
 
     render() {
         return(
             <div>
-                <p>IM HOME</p>
+            <h2>Draw A {this.getRandomWord()}</h2>
+            <div className='canvas-container' align='center'>
+                <CanvasDraw 
+                    ref={canvasDraw => (this.saveableCanvas = canvasDraw)}
+                    brushColor={this.state.color}
+                    canvasWidth={this.state.width}
+                    canvasHeight={this.state.height}
+                />
+                </div>
+                <button onClick={() => {this.setState({"savedCanvas": this.saveableCanvas.getSaveData()})}}>Send To Friend</button>
             </div>
         )
     }
