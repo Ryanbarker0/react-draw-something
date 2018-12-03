3c1767204204453b835b43e66bfc9ab26f0c4ffb
Ryan <ryangeorgebarker0@gmail.com>
2018-12-03 10:36:28 +0000

    format

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -30,9 +30,8 @@ class Signup extends Component {
             password: this.state.password,
             password_confirmation: this.state.password_confirmation
         }
-        this.createUser(newUser)
+    this.createUser(newUser)
     }
-    
     
     render () {
 
@@ -40,31 +39,11 @@ class Signup extends Component {
          <div>
          <h2>Sign Up</h2>
                 <form onSubmit={event => this.passwordCheckAndCreateUser(event)}>
-            <input type='text' placeholder='First Name' onChange={event => this.setState(
-                { 
-                    first_name: event.target.value 
-                }
-                )}/>
-                    <input type='text' placeholder='Last Name' onChange={event => this.setState(
-                        {
-                                last_name: event.target.value
-                            }
-                    )}/>
-                    <input type='text' placeholder='Username' onChange={event => this.setState(
-                        {
-                                username: event.target.value
-                            }
-                    )}/>
-                    <input type='password' placeholder='Password' onChange={event => this.setState(
-                        {
-                                password: event.target.value
-                            }
-                    )}/>
-                    <input type='password' placeholder='Confirm Password' onChange={event => this.setState(
-                        {
-                                password_confirmation: event.target.value
-                            }
-                    )}/>
+            <input type='text' placeholder='First Name' onChange={event => this.setState({first_name: event.target.value})}/>
+                    <input type='text' placeholder='Last Name' onChange={event => this.setState({last_name: event.target.value})}/>
+                    <input type='text' placeholder='Username' onChange={event => this.setState({username: event.target.value})}/>
+                    <input type='password' placeholder='Password' onChange={event => this.setState({password: event.target.value})}/>
+                    <input type='password' placeholder='Confirm Password' onChange={event => this.setState({password_confirmation: event.target.value})}/>
                     <input type='submit' value='Sign Up' />
          </form>
          </div>
